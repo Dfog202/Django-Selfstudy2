ed22f3cbb5127981d83a7bad1f3157055eca3a35
Seungha Lee <poopa8788@gmail.com>
2017-05-30 21:15:43 +0900

    remove hardcore URL, URL names

--- django_app/polls/views.py
+++ django_app/polls/views.py
@@ -9,11 +9,12 @@ def index(request):
     context = {
         'latest_question_list': latest_question_list,
     }
+    print(context)
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pulls.detail.html', {'question': question})
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the result of question %s."
