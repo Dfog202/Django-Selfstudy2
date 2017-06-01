96d9fe2b854d39c9e193fb7e3b9cf083416387d4
Seungha Lee <poopa8788@gmail.com>
2017-06-01 20:04:27 +0900

    customize admin change list, add pub_date filter

--- django_app/polls/admin.py
+++ django_app/polls/admin.py
@@ -14,5 +14,7 @@ class QuestionAdmin(admin.ModelAdmin):
                               'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
 
 admin.site.register(Question, QuestionAdmin)
